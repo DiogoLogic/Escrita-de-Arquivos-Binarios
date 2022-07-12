49e12d490c4a1ca79e13149add70dc1c2e89be28
DiogoLogic <diogoinformatica1@gmail.com>
2022-07-12 01:29:02 -0300

    Gerando arquivo

--- Escrita de arquivos Binários/Program.cs
+++ Escrita de arquivos Binários/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace Escrita_de_arquivos_Binários
 {
@@ -10,6 +13,12 @@ namespace Escrita_de_arquivos_Binários
     {
         static void Main(string[] args)
         {
+            FileStream stream = new FileStream("C:\\estudos c#\\meuarquivo.Diogo",FileMode.OpenOrCreate);
+            BinaryFormatter encoder = new BinaryFormatter();
+
+            encoder.Serialize(stream, 120);
+            encoder.Serialize(stream, "Diogo Felipe");
+            stream.Close();
         }
     }
 }

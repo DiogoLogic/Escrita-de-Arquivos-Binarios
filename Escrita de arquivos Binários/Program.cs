911dadae0acfa1a899553f018287506c03915cf9
DiogoLogic <diogoinformatica1@gmail.com>
2022-07-12 01:18:32 -0300

    Adicionar arquivos de projeto.

---
+++ Escrita de arquivos Binários/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escrita_de_arquivos_Binários
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
